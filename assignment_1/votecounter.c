2d553ba9edd22edd07eba208b898e94d219de18c
Kris Swann <swann013@umn.edu>
2018-02-12 12:01:56 -0600

    Get basic node initalization up and working

--- assignment_1/votecounter.c
+++ assignment_1/votecounter.c
@@ -17,20 +17,43 @@
 
 /** Return if the given line is a comment (starts with a '#') */
 int line_is_comment(char* line) {
-    return line[0] == '#';  // TODO: Trim whitespace
+    return line[0] == '#';
 }
 
 /**
- * Parse "Line 2" of the input file creating the nodes and initalizing their values
+ * Parse "Line 2" of the input file creating the nodes and initializing their values
  * Args:
  *   'line' - Line to parse
  *   'nodes' - Pointer to nodes to be allocated
- *   'candidates' - string of candidates to be passed used in node value initalization
- * */
-void parseInputCreateNodes(char* line, node_t *nodes, char* candidates) {
-    char** argvp = (char**)malloc(MAX_NODES*1024*sizeof(char));
-    int num_tokens = makeargv(line, " ", &argvp);
+ *   'candidates' - string of candidates to be passed used in node value initialization
+ * Return:
+ *   Number of nodes created
+ * Initialized fields of nodes: id, name, output, candidates (copy of given string)
+ * Defaulted fields of nodes: prog (to "leafcounter"), num_children (to 0), status (to 0),
+ *   pid (to 0)
+ */
+int create_nodes(char* line, node_t *nodes, char* candidates) {
+    char*** argvp = (char***)malloc(MAX_NODES*1024*sizeof(char));
+    int num_tokens = makeargv(line, " ", argvp);
     printf("num_tokens: %d\n", num_tokens);
+    for (int i = 0; i < num_tokens; i++) {
+        nodes[i].id = i;
+        strcpy(nodes[i].name, (*argvp)[i]);
+        strcpy(nodes[i].output, (*argvp)[i]);
+        prepend(nodes[i].output, "Output_");
+        strcpy(nodes[i].candidates, candidates);
+        strcpy(nodes[i].prog, "leafcounter");
+        nodes[i].num_children = 0;
+        nodes[i].status = 0;
+        nodes[i].pid = 0;
+    }
+    for (int i = 0; i < num_tokens; i++) {
+        printf("node[%d]: <id: %d, name: %s, output: %s, candidates: %s, prog: %s, num_children: %d, status: %d, pid: %d>\n",
+            i, nodes[i].id, nodes[i].name, nodes[i].output,
+            nodes[i].candidates, nodes[i].prog, nodes[i].num_children,
+            nodes[i].status, nodes[i].pid);
+    }
+    return num_tokens;
 }
 
 /**Function : parseInput
@@ -56,26 +79,29 @@ int parseInput(char *filename, node_t *nodes) {
     char* buf = (char*)malloc(1024*sizeof(char));
     char* candidates = (char*)malloc(1024*sizeof(char));
     int line_num = 0;
+    int num_nodes_created = 0;
     while (buf = read_line(buf, f)) {
+        buf = trimwhitespace(buf);
         if (line_is_comment(buf)) {  // TODO: Ignore empty lines
             continue;
         }
         line_num++;
         if (line_num <= 0) {
-            printf("There was an error parsing the input file.");
+            printf("There was an error parsing the input file.\n");
             exit(1);
         } else if (line_num == 1) {
             strcpy(candidates, buf);
         } else if (line_num == 2) {
-            parseInputCreateNodes(buf, nodes, candidates);
+            num_nodes_created = create_nodes(buf, nodes, candidates);
         } else {
         }
 
-        printf("line %d: %s", line_num, buf);
+        printf("line %d: %s\n", line_num, buf);
     }
-    printf("candidates: %s", candidates);
+    printf("candidates: %s\n", candidates);
     free(buf);
     free(candidates);
+    return num_nodes_created;
 }
 
 /**Function : parseInputLine
