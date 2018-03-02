61586b38ecd29227cbfa55ea50827f65925bced9
Joseph Tang <tangx254@umn.edu>
2018-03-02 12:09:34 -0600

    Added header files and set up main functions.

--- assignment_2/Aggregate_Votes.c
+++ assignment_2/Aggregate_Votes.c
@@ -1,3 +1,18 @@
-int main(void) {
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <dirent.h>
+#include <string.h>
+
+int main(int argc, char **argv) {
+  if (argc != 2) {
+    printf("Usage: ./Aggregate_Votes <path>\n");
+    exit(1);
+  }
+
   return 0;
 }
