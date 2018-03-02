83936121368cee6ced13e441e2a40f146be0d5e0
Joseph Tang <tangx254@umn.edu>
2018-03-02 12:29:51 -0600

    Added more header files and opendir

--- assignment_2/Aggregate_Votes.c
+++ assignment_2/Aggregate_Votes.c
@@ -7,12 +7,24 @@
 #include <fcntl.h>
 #include <dirent.h>
 #include <string.h>
+#include <errno.h>
+
+#include "util.h"
 
 int main(int argc, char **argv) {
   if (argc != 2) {
     printf("Usage: ./Aggregate_Votes <path>\n");
     exit(1);
   }
+
+  DIR* dir = opendir(argv[1]);
+  if (!dir) {
+    perror("Failed to open initial directory");
+    exit(1);
+  } else {
+    printf("Success!\n");
+  }
+
 
   return 0;
 }
