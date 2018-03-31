da44e25a35d7a6d98dc68564b9a231ec1dd5cc37
kriswithank <kristopherswann@gmail.com>
2018-03-30 21:18:43 -0500

    Run all tests and improve makefile
    The make file should now give better output during compilation.

--- assignment_3/test_decrypt.h
+++ assignment_3/test_decrypt.h
@@ -8,36 +8,50 @@
 
 #define MAX_STRING_LEN 1024
 
-void test_example_from_writeup() {
+void test_decrypt_example_from_writeup() {
+  printf("test_example_from_writeup() ");
+
   char source[] = "YQCCJ";
   char target[MAX_STRING_LEN];
   char expected[] = "ASEEL";
   decrypt(source, target);
   assert(strcmp(target, expected) == 0 &&
       "test_example_from_writeup");
+
+  printf("OK\n");
 }
 
 void test_decrypt_yz() {
+  printf("test_decrypt_yz() ");
+
   char source[] = "ABYZ";
   char target[MAX_STRING_LEN];
   char expected[] = "CDAB";
   decrypt(source, target);
   assert(strcmp(target, expected) == 0 &&
       "test_example_from_writeup");
+
+  printf("OK\n");
 }
 
 void test_decrypt_empty_string() {
+  printf("test_decrypt_empty_string() ");
+
   char source[] = "";
   char target[MAX_STRING_LEN];
   char expected[] = "";
   decrypt(source, target);
   assert(strcmp(target, expected) == 0 &&
       "test_example_from_writeup");
+
+  printf("OK\n");
 }
 
 /** Run the tests in this suite. */
 void test_decrypt_runner() {
-  test_example_from_writeup();
+  test_decrypt_example_from_writeup();
+  test_decrypt_yz();
+  test_decrypt_empty_string();
 }
 
 #endif
