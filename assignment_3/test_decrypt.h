3f8073fe404424732d2975d53e351926eafc2ca5
kriswithank <kristopherswann@gmail.com>
2018-03-30 22:06:30 -0500

    Add more test and remove .o from executable

--- assignment_3/test_decrypt.h
+++ assignment_3/test_decrypt.h
@@ -15,8 +15,7 @@ void test_decrypt_example_from_writeup() {
   char target[MAX_STRING_LEN];
   char expected[] = "ASEEL";
   decrypt(source, target);
-  assert(strcmp(target, expected) == 0 &&
-      "test_example_from_writeup");
+  assert(strcmp(target, expected) == 0 && "test example from writeup");
 
   printf("OK\n");
 }
@@ -24,12 +23,12 @@ void test_decrypt_example_from_writeup() {
 void test_decrypt_yz() {
   printf("test_decrypt_yz() ");
 
-  char source[] = "ABYZ";
+  char source[] = "ABYZabyz";
   char target[MAX_STRING_LEN];
-  char expected[] = "CDAB";
+  char expected[] = "CDABcdab";
   decrypt(source, target);
   assert(strcmp(target, expected) == 0 &&
-      "test_example_from_writeup");
+      "y and z should decrpyt to a and b");
 
   printf("OK\n");
 }
@@ -42,7 +41,46 @@ void test_decrypt_empty_string() {
   char expected[] = "";
   decrypt(source, target);
   assert(strcmp(target, expected) == 0 &&
-      "test_example_from_writeup");
+      "empty string should decrypt to empty string");
+
+  printf("OK\n");
+}
+
+void test_decrypt_numbers() {
+  printf("test_decrypt_numbers() ");
+
+  char source[] = "ABC1234DEF567";
+  char target[MAX_STRING_LEN];
+  char expected[] = "CDE1234FGH567";
+  decrypt(source, target);
+  assert(strcmp(target, expected) == 0 &&
+      "numbers should not change during decryption");
+
+  printf("OK\n");
+}
+
+void test_decrypt_whitespace() {
+  printf("test_decrypt_numbers() ");
+
+  char source[] = "A B C\nD\nE F";
+  char target[MAX_STRING_LEN];
+  char expected[] = "C D E\nF\nG H";
+  decrypt(source, target);
+  assert(strcmp(target, expected) == 0 &&
+      "whitespace should not change during decryption");
+
+  printf("OK\n");
+}
+
+void test_decrypt_special_chars() {
+  printf("test_decrypt_numbers() ");
+
+  char source[] = "AB!@#$%^&(*()(|YZ";
+  char target[MAX_STRING_LEN];
+  char expected[] = "CD!@#$%^&(*()(|AB";
+  decrypt(source, target);
+  assert(strcmp(target, expected) == 0 &&
+      "special characters should not change during decryption");
 
   printf("OK\n");
 }
@@ -52,6 +90,9 @@ void test_decrypt_runner() {
   test_decrypt_example_from_writeup();
   test_decrypt_yz();
   test_decrypt_empty_string();
+  test_decrypt_numbers();
+  test_decrypt_whitespace();
+  test_decrypt_special_chars();
 }
 
 #endif
