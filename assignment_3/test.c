0b777853b41a567f43c214a0580c7adc93661241
kriswithank <kristopherswann@gmail.com>
2018-04-03 20:52:59 -0500

    Add makeargv and tests

--- assignment_3/test.c
+++ assignment_3/test.c
@@ -1,8 +1,10 @@
 #include "test_decrypt.h"
 #include "test_dag.h"
+#include "test_makeargv.h"
 
 int main() {
   test_decrypt_runner();
   test_dag_runner();
+  test_makeargv_runner();
   printf("All tests passed!\n");
 }
