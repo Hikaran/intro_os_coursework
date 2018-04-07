d5a44368f375993db9961a9b190abfefec626764
kriswithank <kristopherswann@gmail.com>
2018-04-07 18:20:59 -0500

    Add custom rm -rf implemntation

---
+++ assignment_3/rmrf.h
@@ -0,0 +1,29 @@
+#ifndef RMRF_H
+#define RMRF_H
+
+#define _XOPEN_SOURCE 500
+#include <stdio.h>
+#include <stdlib.h>
+#include <ftw.h>
+
+/**
+ * An implementation of rm -rf using nftw
+ */
+
+static int rm_helper(const char *pathname, const struct stat *sbuf, int type, struct FTW *ftwb) {
+  if(remove(pathname) < 0) {
+    perror("ERROR: remove for rmfr");
+    exit(1);
+  }
+  return 0;
+}
+
+void rmrf(const char* pathname) {
+  // Traverse file tree in reverse order and remove entries
+  if (nftw(pathname, rm_helper, 100, FTW_DEPTH|FTW_MOUNT|FTW_PHYS) < 0) {
+    perror("ERROR: ntfw in rmrf");
+    exit(1);
+  }
+}
+
+#endif
--- assignment_3/test.c
+++ assignment_3/test.c
@@ -1,10 +1,13 @@
+#define _XOPEN_SOURCE 500
 #include "test_decrypt.h"
 #include "test_dag.h"
 #include "test_makeargv.h"
+#include "test_rmrf.h"
 
 int main() {
   test_decrypt_runner();
   test_dag_runner();
   test_makeargv_runner();
+  test_rmrf_runner();
   printf("All tests passed!\n");
 }
