fbc0ba27d384c925cddff3d19abd54a77b690d8d
kriswithank <kristopherswann@gmail.com>
2018-04-26 18:18:26 -0500

    Fix client print statements

--- assignment_4/client.c
+++ assignment_4/client.c
@@ -21,6 +21,7 @@
 #define MSG_SIZE 256
 #define MAX_LINE_LEN 4096
 #define MAX_PATH_LEN 4096
+#define MAX_IP_ADDR_LEN 500
 
 int main(int argc, char** argv) {
 
@@ -55,6 +56,14 @@ int main(int argc, char** argv) {
   // Connect it.
   if (connect(sock, (struct sockaddr *) &address, sizeof(address)) == 0) {
 
+
+    // Save server ip addr to string for easy reuse
+    char server_ip_addr[MAX_IP_ADDR_LEN];
+    sprintf(server_ip_addr, "%s:%d",
+    inet_ntoa(address.sin_addr), address.sin_port);
+
+    printf("Initiated connection with server at %s\n", server_ip_addr);
+
     // Open the file.
     FILE* file = fopen(reqfilepath, "r");
     if (file == NULL) {
@@ -79,29 +88,26 @@ int main(int argc, char** argv) {
       req_to_str(&req, req_str);
 
       // Send request string to server
+      printf("Sending request to server: %s %s\n", req.code, req.data);
       if (send(sock, (void*)req_str, MSG_SIZE, 0) != MSG_SIZE) {
         fprintf(stderr, "Did not send full msg\n");
         exit(1);
       }
 
-      // TODO: Remove
-      printf("SENT: |%s|\n", req_str);
-
       // Recive response string from server
       char resp_str[MSG_SIZE];
       recv(sock, (void*)resp_str, MSG_SIZE, 0);
 
-      // TODO: REMOVE
-      printf("RECV: |%s|\n", resp_str);
-
       // Convert response string to response struct
       struct response_msg resp;
       parse_resp_msg_str(resp_str, &resp);
+      printf("Recived response from server: %s %s\n", resp.code, resp.data);
     }
 
     // Close the file and socket
     fclose(file);
     close(sock);
+    printf("Closed connection with server at %s\n", server_ip_addr);
   } else {
     perror("Connection failed!");
   }
